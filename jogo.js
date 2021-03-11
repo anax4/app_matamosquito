9c358c634864d82fe1a133f0ad4d1ec233c745b3
Ana Xavier <45177600+anax4@users.noreply.github.com>
2021-03-10 23:39:23 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,11 +1,21 @@
 let altura = 0;
 let largura = 0;
 let vidas = 1;
-let tempo = 5;
+let tempo = 10;
 
-///let criaMosquitoTempo = 1500;
+let criaMosquitoTempo = 1500;
 
-alert(window.location.href);
+let nivel = window.location.search;
+
+//alert(nivel.replace('?', ''));
+
+if (nivel === 'normal') {
+  criaMosquitoTempo = 1500;
+} else if (nivel === 'dificil') {
+  criaMosquitoTempo = 1000;
+} else if (nivel === 'hardcore') {
+  criaMosquitoTempo = 750;
+}
 
 //pega a altura e a largura do browser
 function ajustaTamanhoJogo() {
@@ -21,12 +31,12 @@ let cronom = setInterval(function () {
   tempo -= 1;
   if (tempo < 0) {
     clearInterval(cronometro);
-    clearInterval(criarmosca);
+    clearInterval(criaMosca);
     window.location.href = 'win.html';
   } else {
     document.getElementById('cronometro').innerHTML = tempo;
   }
-}, 1000);
+}, criaMosquitoTempo);
 
 function posicaoRond() {
   //REMOVE O MOSQUITO ANTERIOR
